e5c2444a71b68a74c09afff1e0b5bfb756d2002a
vantrieu <nguyenvantrieu.it@gmail.com>
2020-11-15 17:21:13 +0700

    update generate database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('./helpers/error-handler');
 const Account = require('./models/Account');
+const User = require('./models/User');
 const fileUpload = require('express-fileupload');
 require('dotenv/config');
 
@@ -13,10 +14,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(errorhandler);
 app.use(require('morgan')('combined'))
-app.use(fileUpload({createParentPath: true}));
-app.use(express.static(__dirname+'/public'));
+app.use(fileUpload({ createParentPath: true }));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 //Import Routes
@@ -30,10 +31,10 @@ app.use('/users', userRoute);
 //Connect mongodb
 mongoose.connect(
     process.env.DB_CONNECTION, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    } 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}
 );
 
 mongoose.connection.on('error', console.error.bind(console, 'Database connection error:'));
@@ -44,13 +45,20 @@ mongoose.connection.once('open', function () {
         if (err)
             console.log(err);
         if (!account) {
-            const account = new Account();
-            account._doc.username = 'admin';
+            let account = new Account();
+            let user = new User();
+            account.username = 'admin';
             account.password = '12345678';
             account.email = 'admin@gmail.com';
+            account.user_id = user._id;
             account.role = 'Administrator';
             account.createdate = Date.now();
             account.save();
+            user.firstname = 'Super';
+            user.lastname = 'Admin';
+            user.birthday = Date.now();
+            user.gender = 'Không muốn tiết lộ';
+            user.save();
             console.log('Generate database success!');
         } else {
             console.log('Not generate database!');
